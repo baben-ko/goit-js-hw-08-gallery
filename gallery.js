06727aec6800d03bad188ad3210eb9a39e30ba76
Andrii_Babenko <andrey.brnr@gmail.com>
2021-02-01 21:08:28 +0200

    DONE

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 import items from './gallery-items.js';
 
-// console.table(items);
+
 
 const createGalleryMarkup = item => {
 
@@ -26,8 +26,57 @@ return liRef;
 const galleryCards = items.map(item => createGalleryMarkup(item));
 
 const galleryUlRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
+// const imgRef = document.querySelector('.gallery__image');
 
 galleryUlRef.append(...galleryCards);
 
+// -----------------------------
+
+galleryUlRef.addEventListener('click', onClick);
+
+function onClick(event) {
+
+    event.preventDefault();
+
+    const tagBtn = event.target;
+
+   if(tagBtn.nodeName !== 'IMG'){
+       console.log('клик не по картинке!!!');
+       return;  
+   }
+
+  lightboxRef.classList.add('is-open');
+
+  lightboxImage.src = tagBtn.dataset.source;  
+
+//   console.log(object);
+   
+}
+
+    // закрытие модалки по Х
+
+const closeModalBtn = document.querySelector('button[data-action="close-lightbox"]')
+
+closeModalBtn.addEventListener('click', () => {
+
+    lightboxRef.classList.remove('is-open');
+    lightboxImage.src = '';
+});
+
+// закрытие модалки по бекдропу
+
+lightboxOverlayRef.addEventListener('click', event => {
+
+    if(event.target === event.currentTarget) {
+
+        lightboxRef.classList.remove('is-open');
+        lightboxImage.src = '';
+
+    }
+
+});
 
 
